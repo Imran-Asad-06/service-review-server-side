13272123f03235cebd7544ad1f04069b9df1b4a2
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-10 02:58:55 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Photography assignment ')
+})
+
+app.listen(port, () => {
+    console.log(`Photography assignment ${port}`);
+})
