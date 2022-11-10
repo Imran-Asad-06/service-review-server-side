d41e3a15d6425766d478b5b6a0c6bda71dd805ac
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-10 16:36:44 +0600

    details api with id

--- index.js
+++ index.js
@@ -14,11 +14,47 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.stfbiry.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photography').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        });
+        app.get('/allServices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+        app.post('/allServices', async (req, res) => {
+            const Service = req.body;
+            const result = await serviceCollection.insertOne(Service);
+            res.send(result);
+            });
+            
+            app.get('/allServices/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            console.log(service)
+            res.send(service);
+            });
+            
+            
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
