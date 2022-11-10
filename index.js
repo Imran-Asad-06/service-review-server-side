3ef67d2a86e479595c73d07449faacbff1ee327d
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-11-10 19:01:15 +0600

    post method added

--- index.js
+++ index.js
@@ -39,13 +39,12 @@ async function run(){
             res.send(result);
             });
             
-            app.get('/allServices/:id', async (req, res) => {
+        app.get('/allServices/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
-            console.log(service)
             res.send(service);
-            });
+        });
             
             
     }
